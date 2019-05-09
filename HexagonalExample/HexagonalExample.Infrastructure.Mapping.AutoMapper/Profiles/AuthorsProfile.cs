9f9e287cb9568831a0736029cea05b7971c55c26
Konstantin Shvyryaev <konstantin.shvyryaev@yandex.ru>
2019-05-09 16:42:39 +0400

    Mappings for entity framework models was added

--- HexagonalExample/HexagonalExample.Infrastructure.Mapping.AutoMapper/Profiles/AuthorsProfile.cs
+++ HexagonalExample/HexagonalExample.Infrastructure.Mapping.AutoMapper/Profiles/AuthorsProfile.cs
@@ -2,6 +2,7 @@
 using HexagonalExample.Api.Models.Commands.Authors;
 using HexagonalExample.Api.Models.Responses;
 using HexagonalExample.Domain.Entities;
+using HexagonalExample.Infrastructure.Data.EF.Models;
 using HexagonalExample.Infrastructure.Data.Mongo.Models;
 
 namespace HexagonalExample.Infrastructure.Mapping.AutoMapper.Profiles
@@ -10,9 +11,21 @@ namespace HexagonalExample.Infrastructure.Mapping.AutoMapper.Profiles
     {
         public AuthorsProfile()
         {
+            // Mongo
             CreateMap<Author, AuthorMongoModel>();
             CreateMap<AuthorMongoModel, Author>();
 
+            // Entity Framework
+            CreateMap<Author, AuthorEFModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(x =>
+                    string.IsNullOrWhiteSpace(x.Id)
+                        ? default(int)
+                        : int.Parse(x.Id)))
+                .ForMember(dest => dest.BookId, opt => opt.Ignore())
+                .ForMember(dest => dest.Book, opt => opt.Ignore());
+            CreateMap<AuthorEFModel, Author>();
+
+            // Api
             CreateMap<Author, AuthorResponse>();
             CreateMap<CreateAuthorCommand, Author>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
--- HexagonalExample/HexagonalExample.Infrastructure.Mapping.AutoMapper/Profiles/BooksProfile.cs
+++ HexagonalExample/HexagonalExample.Infrastructure.Mapping.AutoMapper/Profiles/BooksProfile.cs
@@ -2,6 +2,7 @@
 using HexagonalExample.Api.Models.Commands.Books;
 using HexagonalExample.Api.Models.Responses;
 using HexagonalExample.Domain.Entities;
+using HexagonalExample.Infrastructure.Data.EF.Models;
 using HexagonalExample.Infrastructure.Data.Mongo.Models;
 
 namespace HexagonalExample.Infrastructure.Mapping.AutoMapper.Profiles
@@ -10,9 +11,19 @@ namespace HexagonalExample.Infrastructure.Mapping.AutoMapper.Profiles
     {
         public BooksProfile()
         {
+            // Mongo
             CreateMap<Book, BookMongoModel>();
             CreateMap<BookMongoModel, Book>();
 
+            // Entity Framework
+            CreateMap<Book, BookEFModel>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(x =>
+                    string.IsNullOrWhiteSpace(x.Id)
+                        ? default(int)
+                        : int.Parse(x.Id)));
+            CreateMap<BookEFModel, Book>();
+
+            // Api
             CreateMap<Book, BookResponse>();
             CreateMap<CreateBookCommand, Book>()
                 .ForMember(dest => dest.Id, opt => opt.Ignore());
